97457f48894b10949a9e669df747320c8dabaa3f
Lem Uhuru <lem@esy.com>
2024-11-11 20:03:34 -0500

    update meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,10 +23,10 @@ const lexend = localFont({
 export const metadata = {
   title: {
     template: '%s - Docs',
-    default: 'CacheAdvance - Never miss the cache again.',
+    default: 'WordToken - Guides, tutorials, and recipes for prompt engineering.',
   },
   description:
-    'Cache every single thing your app could ever do ahead of time, so your code never even has to run at all.',
+    'Guides, tutorials, and recipes for prompt engineering.',
 }
 
 export default function RootLayout({ children }) {
